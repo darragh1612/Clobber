c59d9522093dce9d6f4e6bec4fee5512adaaa8a5
darragh1612 <x17469074@student.ncirl.ie>
2019-11-24 23:53:36 +0000

    Initial commit

---
+++ app/src/main/java/com/example/paypal/config/Config.java
@@ -0,0 +1,5 @@
+package com.example.paypal.config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "AaWoqMLA5pIWavXC4yJSrE6Nz2EE7W-6S9cAPbgyHm3PZgXVrVM142om7HNZ0BJlD4DfOY756v4HwWGw";
+}
